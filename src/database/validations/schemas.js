2f72a25575dac020187b86dde81676774c57b721
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-21 22:22:19 -0300

    Add POST /signature route and GET /products routes

--- src/database/validations/schemas.js
+++ src/database/validations/schemas.js
@@ -1,10 +1,22 @@
 import joi from "joi";
-import { emailRegex, strongPasswordRegex } from "./regex.js";
+import { emailRegex, strongPasswordRegex, cepRegex } from "./regex.js";
 
 const usersSchema = joi.object({
-	name: joi.string().min(3).required(),
-	email: joi.string().pattern(new RegExp(emailRegex)).required(),
-	password: joi.string().pattern(new RegExp(strongPasswordRegex)).required(),
+    name: joi.string().min(3).required(),
+    email: joi.string().pattern(new RegExp(emailRegex)).required(),
+    password: joi.string().pattern(new RegExp(strongPasswordRegex)).required(),
 });
 
-export { usersSchema };
+const signatureSchema = joi.object({
+    userId: joi.number().min(1).required(),
+    cep: joi.string().max(8).pattern(new RegExp(cepRegex)).required(),
+    city: joi.string().required(),
+    startDate: joi.date().greater("now").iso().required(),
+    deliveryAddress: joi.string().required(),
+    plan: joi.string().required(),
+    products: joi.array().required(),
+    state: joi.string().min(2).max(2).required(),
+    userFullName: joi.string().required(),
+});
+
+export { usersSchema, signatureSchema };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { signUp, signIn } from "./controllers/usersController.js";
 import { getPlans } from "./controllers/plansController.js";
 import { sendOk } from "./helpers/responses.js";
 import auth from "./middleware/auth.js";
+import { signPlan } from "./controllers/signatureController.js";
+import { getProducts } from "./controllers/productsController.js";
 
 const routes = Router();
 
@@ -10,5 +12,7 @@ routes.post("/sign-up", signUp);
 routes.post("/sign-in", signIn);
 routes.get("/auth", auth, sendOk);
 routes.get("/plans", auth, getPlans);
+routes.get("/products", auth, getProducts);
+routes.post("/signature", auth, signPlan);
 
 export default routes;
