242458bc80d42967ec51fee7c70af2716986929c
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-22 00:41:12 -0300

    Add GET /signature route

--- src/controllers/signatureController.js
+++ src/controllers/signatureController.js
@@ -50,6 +50,22 @@ async function signPlan(req, res) {
             [userId, plan, startDate, deliveryInfo.rows[0].id]
         );
 
+        /* construct INSERT products query dinamically */
+        const insertProducts = [];
+        let preparedQuery = "";
+        products.forEach((product, index) => {
+            insertProducts.push(signature.rows[0].id, product);
+            preparedQuery += `($${(index + 1) * 2 - 1}, $${(index + 1) * 2})`;
+
+            if (products.length > 1 && index + 1 < products.length)
+                preparedQuery += ", ";
+        });
+
+        await connection.query(
+            `INSERT INTO signature_products (signature_id, product_id) VALUES ${preparedQuery}`,
+            insertProducts
+        );
+
         res.status(201).send({
             signatureId: signature.rows[0].id,
             userId,
@@ -68,4 +84,53 @@ async function signPlan(req, res) {
     }
 }
 
-export { signPlan };
+async function getUserPlan(req, res) {
+    const { authorization } = req.headers;
+    const token = authorization.replace("Bearer ", "");
+    try {
+        const result = await connection.query(
+            `SELECT signatures.id,
+                    sessions.user_id AS "userId",
+                    signatures.start_date AS "startDate",
+                    delivery_info.user_fulllname AS "userFullName",
+                    delivery_info.address AS "deliveryAddress",
+                    delivery_info.cep,
+                    delivery_info.city,
+                    delivery_info.state,
+                    plans.id AS "planId",
+                    plans.name AS "planName",
+                    plans.period AS "planPeriod"
+            FROM sessions
+            JOIN signatures 
+                ON signatures.user_id = sessions.user_id
+            JOIN delivery_info
+                ON signatures.delivery_info = delivery_info.id
+            JOIN plans
+                ON plans.id = signatures.plan_id
+            WHERE sessions.token = $1`,
+            [token]
+        );
+        const signature = result.rows[0];
+
+        const signatureProducts = await connection.query(
+            `SELECT products.name 
+            FROM products
+            JOIN signature_products
+                ON signature_products.product_id = products.id
+            WHERE signature_products.signature_id = $1`,
+            [signature.id]
+        );
+
+        signature.products = [];
+        signatureProducts.rows.forEach((product) => {
+            signature.products.push(product.name);
+        });
+
+        res.send(signature);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export { signPlan, getUserPlan };
