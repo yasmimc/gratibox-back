1fac879fdda3f818665a2cee7335cbb285b90acc
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-25 21:16:19 -0300

    Add createSignature factory

--- src/database/validations/schemas.js
+++ src/database/validations/schemas.js
@@ -1,4 +1,3 @@
-import dayjs from "dayjs";
 import joi from "joi";
 import { emailRegex, strongPasswordRegex, cepRegex } from "./regex.js";
 
@@ -12,11 +11,7 @@ const signatureSchema = joi.object({
     userId: joi.number().min(1).required(),
     cep: joi.string().max(8).pattern(new RegExp(cepRegex)).required(),
     city: joi.string().required(),
-    startDate: joi
-        .date()
-        .greater(dayjs().set("hour", 0).set("minute", 0).set("second", 0))
-        .iso()
-        .required(),
+    startDate: joi.date().iso().required(),
     deliveryAddress: joi.string().required(),
     plan: joi.string().required(),
     products: joi.array().required(),
